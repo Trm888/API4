e9a1d20171c8ade6eca2be43e13c80427d13b527
Trm888 <bigtram888@gmail.com>
2022-09-20 15:38:46 +0900

    publish photo to Telegram channel.

--- telegramm_bot.py
+++ telegramm_bot.py
@@ -7,3 +7,5 @@ bot = telegram.Bot(token=TOKEN)
 print(bot.get_me())
 
 bot.send_message(chat_id='@alis_devman', text="I'm sorry Dave I'm afraid I can't do that.")
+
+bot.send_document(chat_id='@alis_devman', document=open('image/NASA_EPIC_3.png', 'rb'))
